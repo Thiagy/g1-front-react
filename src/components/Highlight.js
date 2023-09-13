8f76e6b5159d3fe9337ee5654e730720b7e2a3a5
Thiago Rodrigues Teixeira <thiagotx2019@gmail.com>
2023-09-13 08:02:48 -0300

    ok

--- src/components/Highlight.js
+++ src/components/Highlight.js
@@ -1,102 +1,88 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
-export default function Highlight({news_1, news_2, news_3}){
-
+export default function Highlight({ news_1, news_2, news_3 }) {
   const highlight_1 = {
-   
     height: "100%",
     width: "49.5%",
     position: "absolute",
     left: 0,
-    borderRadius: "14px"
+    borderRadius: "14px",
+  };
 
-  }
   const highlight_2 = {
-      
-      height: "49.5%",
-      width: "50%",
-      position: "absolute",
-      top: 0,
-      right: 0,
-      borderRadius: "14px"
-      
-  }
-  const highlight_3 = {
-      
-      height: "49.5%",
-      width: "50%",
-      position: "absolute",
-      bottom: 0,
-      right: 0,
-      borderRadius: "14px",
+    height: "49.5%",
+    width: "50%",
+    position: "absolute",
+    top: 0,
+    right: 0,
+    borderRadius: "14px",
+  };
 
-  }
+  const highlight_3 = {
+    height: "49.5%",
+    width: "50%",
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    borderRadius: "14px",
+  };
+
   const img_highlight = {
-      height: '100%',
-      width: '100%',
-      borderRadius: '14px'
-  }
+    height: "100%",
+    width: "100%",
+    borderRadius: "14px",
+  };
+
   const title_highlight = {
-      top: '10px',
-      left: '10px',
-      position: 'absolute'
-  }
+    top: "10px",
+    left: "10px",
+    position: "absolute",
+  };
 
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  
+
   useEffect(() => {
-      function handleResize() {
-        setWindowWidth(window.innerWidth);
-      }
-  
-      window.addEventListener("resize", handleResize);
-  
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
-  
+
   // Lógica para determinar se a largura da tela é menor ou igual a 768px
   const isSmallScreen = windowWidth <= 768;
-  
-  const csshighlight = {/*Div de notícias de destaque*/
 
+  const csshighlight = {
+    /*Div de notícias de destaque*/
     height: isSmallScreen ? "50vh" : "78vh",
     width: isSmallScreen ? "100%" : "95%", // Altera a largura para 100% se a tela for pequena
-    position: "relative"
-
-  }
+    position: "relative",
+  };
 
   return (
-
     <div style={csshighlight} id="csshighlight">
-
+      {news_1.length > 0 && (
         <a href="#" target="_blank" style={highlight_1}>
-
-            <h4 style={title_highlight} >{news_1[0].content}</h4>
-
-            <img style={img_highlight} src={news_1[0].image}/>
-
+          <h4 style={title_highlight}>{news_1[0].content}</h4>
+          <img style={img_highlight} src={news_1[0].image} />
         </a>
-
+      )}
+      {news_2.length > 1 && (
         <a href="#" target="_blank" style={highlight_2}>
-
-            <h4 style={title_highlight}>{news_2[0].content}</h4>
-
-            <img style={img_highlight} src={news_2[0].image}/>
-
+          <h4 style={title_highlight}>{news_2[1].content}</h4>
+          <img style={img_highlight} src={news_2[1].image} />
         </a>
-
+      )}
+      {news_3.length > 2 && (
         <a href="#" target="_blank" style={highlight_3}>
-
-            <h4 style={title_highlight}>{news_3[0].content}</h4>
-
-            <img style={img_highlight} src={news_3[0].image}/>
-
+          <h4 style={title_highlight}>{news_3[2].content}</h4>
+          <img style={img_highlight} src={news_3[2].image} />
         </a>
-            
+      )}
     </div>
-
   );
 }
-
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,20 +5,17 @@ import BoxNew from "./BoxNew";
 import New from './New';
 import { getNews } from "./functions/newApi";
 
-export default function Main(){
-
+export default function Main() {
   const [news, setNews] = useState([]);
-  const [index_news, setIndexNews] = useState(0); // Use o estado para armazenar o índice
+  const [index_news, setIndexNews] = useState(0);
+  const [visibleNewsCount, setVisibleNewsCount] = useState(8);
 
   async function fetchNews() {
-    
     const footer = document.querySelector('footer')
-
     const newsData = await getNews();
 
     if (newsData) {
-       
-        footer.style.display='flex'
+      footer.style.display = 'flex';
     }
     setNews(newsData);
   }
@@ -27,7 +24,6 @@ export default function Main(){
     fetchNews();
   }, []); 
 
-  // Atualize o índice dentro de setInterval usando o estado
   useEffect(() => {
     const interval = setInterval(() => {
       if (index_news + 17 < news.length) {
@@ -35,48 +31,53 @@ export default function Main(){
       } else {
         setIndexNews(0);
       }
-    }, 5000); // Intervalo de 5 segundos (ou outro valor desejado)
+    }, 5000);
 
-    return () => clearInterval(interval); // Limpar o intervalo ao desmontar o componente
+    return () => clearInterval(interval);
   }, [index_news, news]);
 
-  // Divida as notícias em grupos de 5
-  const news_1 = news.slice(index_news, index_news + 5);
-  const news_2 = news.slice(index_news + 6, index_news + 11);
-  const news_3 = news.slice(index_news + 12, index_news + 17);
+  const loadMoreNews = () => {
+    setVisibleNewsCount(visibleNewsCount + 5);
+  };
 
   return (
     <>
-    <div id="spinner" style={{display: news.length ? 'none' : 'block'}}></div>
-    <main style={{display: news.length ? 'flex' : 'none'}}>
+      <div id="spinner" style={{ display: news.length ? 'none' : 'block' }}></div>
+      <main style={{ display: news.length ? 'flex' : 'none' }}>
+
+        {/*Componente que exibe os anúncios*/}
         <Marketing/>
+
+        {/*Componente que exibe os notícias de destaque*/}
         {news.length > 0 && (
-        <Highlight
-          news_1={news_1}
-          news_2={news_2}
-          news_3={news_3}
-        />
-      )}
+          <Highlight
+            news_1={news.slice(index_news, index_news + 5)}
+            news_2={news.slice(index_news + 6, index_news + 11)}
+            news_3={news.slice(index_news + 12, index_news + 17)}
+          />
+        )}
+
+        {/*Componente que exibe os anúncios*/}
         <Marketing/>
-        
         <div className="container">
-            <div className="container1">
-                {news.map((item, index) => (
-                  <New
-                    key={index} 
-                    title={item.title}
-                    content={item.content}
-                    image={item.image}
-                  />
-                ))}
-            </div>
-            <div className="container2">
-              <BoxNew newarray={news_1} title='Você viu isso?'/>
-              <BoxNew newarray={news_2}  title='Blogs e colunas'/>
-              <BoxNew newarray={news_3} title='Mais lidas'/>
-            </div>  
+          {/*Componente que exibe os lista de notícias*/}
+          <div className="container1">
+            {news.slice(0, visibleNewsCount).map((item, index) => (
+              <New key={index} title={item.title} content={item.content} image={item.image} />
+            ))}
+            {visibleNewsCount < news.length && (
+              <div id="btn_show_more" onClick={loadMoreNews}>Veja mais</div>
+            )}
+          </div>
+          <div className="container2">
+          {/*Componentes que exibe os lista de notícias secundárias*/}
+
+            <BoxNew newarray={news.slice(index_news, index_news + 5)} title='Você viu isso?'/>
+            <BoxNew newarray={news.slice(index_news + 6, index_news + 11)} title='Blogs e colunas'/>
+            <BoxNew newarray={news.slice(index_news + 12, index_news + 17)} title='Mais lidas'/>
+          </div>  
         </div>     
-    </main>
+      </main>
     </>
   );
 }
